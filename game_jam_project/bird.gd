90dfd7232315e75488a58bcbca6592d1d5c9cc20
Cayuga007 <jayson.tram13@gmail.com>
2025-06-28 11:01:02 -0700

    obstacle generation

---
+++ game_jam_project/bird.gd
@@ -0,0 +1,13 @@
+class_name Bird extends Obstacle
+
+
+const BIRD_TRAVEL_SPEED = 200
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position.x -= BIRD_TRAVEL_SPEED * delta
